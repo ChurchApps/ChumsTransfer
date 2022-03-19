5d177724758d52c7ebc38d9d2fe9ce58d843ca4e
Jeremy Zongker <jeremy@trilitech.com>
2022-03-19 09:24:11 -0500

    Updated footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer: React.FC = () => (
   <div id="footer">
     <div className="text-center">
-      <img src="/images/logo.png" alt="logo" />
+      <img src="/images/logo-footer.png" alt="logo" style={{ maxWidth: 300 }} />
       <p>Phone: 918-994-2638 &nbsp; | &nbsp; support@chums.org</p>
       <p>2022 © Live Church Solutions. All rights reserved.</p>
     </div>
